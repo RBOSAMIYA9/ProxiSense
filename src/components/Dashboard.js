143d5a0bee01bfe7d06f9eb7075f385fdd01457a
Ravindra <rbosamiya9@gmail.com>
2021-11-20 23:15:42 +0530

    updating to old mqtt broker url

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,8 +29,8 @@ function Dashboard({ user, setUser }) {
         console.log("inside use effect");
         // const client = mqtt.connect("ws://broker.hivemq.com:8000");
         // const client = mqtt.connect("ws://broker.mqttdashboard.com:8000/mqtt");
-        const client = mqtt.connect("wss://broker.emqx.io:8084/mqtt");
-        // const client = mqtt.connect("ws://test.mosquitto.org:8081");
+        // const client = mqtt.connect("wss://broker.emqx.io:8084/mqtt");
+        const client = mqtt.connect("ws://test.mosquitto.org:8080");
         
         client.on('connect', () => {
             setMqttStatus(true);
